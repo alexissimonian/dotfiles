8c3ac6c16507a647ded74fe95a58b240018a6156
Alexis <alexis.simonian@outlook.com>
2025-09-18 09:37:32 +0000

    lsp

--- .config/nvim/lua/config/lazy/telescope.lua
+++ .config/nvim/lua/config/lazy/telescope.lua
@@ -8,10 +8,20 @@ return {
 
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>fF', function()
+			builtin.find_files({ hidden = true })
+		end, {})
 		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>fw', function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set('n', '<leader>fW', function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set('n', '<leader>fs', function()
+			builtin.live_grep()
 		end)
 	end
 }
